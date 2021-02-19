68683d893857db5377cd2475a2ff8a9098d94aac
Dylan Park <dylanpark.design@gmail.com>
2021-02-19 13:04:42 +1000

    Update WeatherManager.swift

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = https://api.openweathermap.org/data/2.5/weather?appid=ede5a97a44bcb66b24d4da392ba91df2&units=metric
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=ede5a97a44bcb66b24d4da392ba91df2&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
