cdabe3b96113b6c0e0af5dc13d1ddb38fba66187
Dylan Park <dylanpark.design@gmail.com>
2021-02-20 13:15:08 +1000

    Update WeatherManager.swift

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -17,33 +17,30 @@ struct WeatherManager {
     }
     
     func performRequest(urlString: String) {
-        
-        //1. Create a URL
+        //1. Create URL
         if let url = URL(string: urlString) {
             
             //2. Create a URLSession
             let session = URLSession(configuration: .default)
             
-            //3. Give the URLSession a Task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+                
+            }
             
             //4. Start the task
             task.resume()
         }
     }
-    
-    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
-        if error != nil {
-            print(error!)
-            return
-            //return = Exit the function
-        }
         
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
-        
-    }
     
 }
