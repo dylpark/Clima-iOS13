361bec870b957f75a58c071861460ec98caceb0b
Dylan Park <dylanpark.design@gmail.com>
2021-02-23 13:25:59 +1000

    Update WeatherModel.swift

--- Clima/Model/WeatherModel.swift
+++ Clima/Model/WeatherModel.swift
@@ -17,7 +17,6 @@ struct WeatherModel {
         return String(format: "%.1f", temperature)
     }
     
-    //Computed Property
     var conditionName: String {
         switch conditionId {
         case 200...232:
